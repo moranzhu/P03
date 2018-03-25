8ee491d886495d00ebefe9bdb653e0a452be7918
Unknown <momo.zhu0721@gmail.com>
2018-03-24 19:58:06 -0700

    add bookcover, text of the projects are responsive

--- js/changeCover.js
+++ js/changeCover.js
@@ -1,8 +1,32 @@
 "use strict";
 
-document.getElementById("selected").onmouseover = function(){
+document.getElementById("enroute").onmouseover = function(){
   document.getElementById("cover").src="img/enrouteCover.png";
+
+  var aroma = document.getElementById("aroma");
+  aroma.style.color = "#B2B2B2";
+  var enroute = document.getElementById("enroute");
+  enroute.style.color = "#333";
+  var book = document.getElementById("book");
+  book.style.color = "#B2B2B2";
 }
 document.getElementById("aroma").onmouseover = function(){
   document.getElementById("cover").src="img/aromaCover.png";
+
+  var aroma = document.getElementById("aroma");
+  aroma.style.color = "#333";
+  var enroute = document.getElementById("enroute");
+  enroute.style.color = "#B2B2B2";
+  var book = document.getElementById("book");
+  book.style.color = "#B2B2B2";
+}
+document.getElementById("book").onmouseover = function(){
+  document.getElementById("cover").src="img/bookCover.png";
+
+  var aroma = document.getElementById("aroma");
+  aroma.style.color = "#B2B2B2";
+  var enroute = document.getElementById("enroute");
+  enroute.style.color = "#B2B2B2";
+  var book = document.getElementById("book");
+  book.style.color = "#333";
 }
